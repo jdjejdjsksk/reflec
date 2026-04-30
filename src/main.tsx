d5e2c78fae88f91d284c8832c0f2f3f5a0011828
jdjejdjsksk <lukexxa@gmail.com>
2026-04-30 17:24:52 +0400

    main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
+
+function App() {
+  return (
+    <main>
+      <h1>reflec.xyz</h1>
+      <p>Roblox scripting engineer building gameplay systems, UI, and optimized experiences.</p>
+    </main>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
